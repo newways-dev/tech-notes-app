9be6f383ea20c106e6d46450b903c87dd0e57d42
newways-dev <reactwebdew@gmail.com>
2023-01-03 13:07:53 +0300

    middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const path = require('path')
-
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const corsOptions = require('./config/corsOptions')
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
 // Middleware
+app.use(logger)
 app.use(express.json())
+app.use(cookieParser())
+app.use(cors(corsOptions))
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root.js'))
@@ -21,5 +28,7 @@ app.all('*', (req, res) => {
     res.type('txt').send('404 Not Found')
   }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
